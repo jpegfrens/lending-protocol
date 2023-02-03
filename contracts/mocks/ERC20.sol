3cce33245a5cbd1e21c0c08671adca9a55d1d929
Leif <56021265+etherleif@users.noreply.github.com>
2023-02-03 15:15:28 +0100

    making mock erc20 mintable

--- contracts/mocks/ERC20.sol
+++ contracts/mocks/ERC20.sol
@@ -2,15 +2,24 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MOCK20 is ERC20 {
+contract MOCK20 is ERC20, Ownable {
     uint8 private _decimals;
 
-    constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol) {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) ERC20(name, symbol) {
         _decimals = decimals;
     }
 
     function decimals() public view virtual override returns (uint8) {
         return _decimals;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
